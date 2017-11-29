4337c2e2b44a83ac1fc321cca36e1a7b51b27ec4
tewksbg <GregATewksbury@gmail.com>
2017-11-29 14:24:03 -0500

    Added a new source
    Added localhost ads

--- BTC/js/addresses.js
+++ BTC/js/addresses.js
@@ -1,4 +1,5 @@
 var s = [
   "http://moonbit.co.in/?ref=dbaba97e6169",
-  "https://99bitcoins.com/bitcoin-faucet/?r=" + ref
+  "https://99bitcoins.com/bitcoin-faucet/?r=" + ref,
+  "https://actioncommandfinancediv.github.io/AltCoinRotator/BTC/ads/ads-misc.html"
 ];
