5338e11ad5eb3bf6e7399be1215f113ade57dd1e
tewksbg <GregATewksbury@gmail.com>
2017-11-30 14:58:26 -0500

    Added new faucets

--- BTC/js/addresses.js
+++ BTC/js/addresses.js
@@ -1,5 +1,7 @@
 var s = [
   "http://moonbit.co.in/?ref=dbaba97e6169",
   "https://99bitcoins.com/bitcoin-faucet/?r=" + ref,
-  "https://actioncommandfinancediv.github.io/AltCoinRotator/BTC/ads/ads-misc.html"
+  "https://actioncommandfinancediv.github.io/AltCoinRotator/BTC/ads/ads-misc.html",
+  "https://freebitco.in/?r=10295743",
+  "https://www.trustbtcfaucet.com/?ref=alookatcybercoins"
 ];
